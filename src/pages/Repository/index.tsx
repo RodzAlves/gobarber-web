f15a4b0094add02b901cbfa41e74fdb45dea1a3e
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-01-17 16:12:44 -0300

    feat: creating repository page.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Container, Header, RepositoryInfo, Issues } from './styles';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
   return (
     <Container>
-      <h1>Repository</h1>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/68624711?s=460&u=82dded1c5330835aa860a3053f3c62603ea97ca3&v=4"
+            alt="Teste"
+          />
+          <div>
+            <strong>RodzAlves/gobarber</strong>
+            <p>Descrição do repositório</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/asdasdsd">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdasdasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </Container>
   );
 };
