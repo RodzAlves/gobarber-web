97847652c773fc8facc7ca8e73b8520da0aa7b3f
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-01-17 15:37:24 -0300

    feat: dealing with errors in the search.

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Container = styled.div``;
 
@@ -11,7 +15,7 @@ export const Title = styled.h1`
   line-height: 56px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -22,6 +26,8 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid ${(props) => (props.hasError ? '#c53030' : '#fff')};
+    border-right: 0;
 
     &::placeholder {
       color: #a8a8b3;
@@ -41,6 +47,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
