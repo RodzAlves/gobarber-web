c9050a60480f967e79ba610c417ebb53ed193713
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-01-19 12:51:34 -0300

    feat: Sign Up page created.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,12 +11,15 @@ import { Container, Background, Content } from './styles';
 const SignIn: React.FC = () => {
   return (
     <Container>
+      <Background />
+
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
         <form>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu cadastro</h1>
 
+          <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
             name="password"
@@ -25,17 +28,14 @@ const SignIn: React.FC = () => {
             type="password"
           />
 
-          <Button type="submit">Entrar</Button>
-
-          <a href="forgot">Esqueci minha senha</a>
+          <Button type="submit">Cadastrar</Button>
         </form>
 
-        <a href="signup">
-          <FiLogIn />
-          Criar conta
+        <a href="/">
+          <FiArrowLeft />
+          Voltar para logon
         </a>
       </Content>
-      <Background />
     </Container>
   );
 };
