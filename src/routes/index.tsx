ce4cd9e67600331834f06375cb0c589a2c912143
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-01-19 11:29:38 -0300

    style: config prettier, eslint and folders.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard/index';
-import Repository from '../pages/Repository/index';
-
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/repository/:repository+" component={Repository} />
+      <Route path="/" />
     </Switch>
   </BrowserRouter>
 );
